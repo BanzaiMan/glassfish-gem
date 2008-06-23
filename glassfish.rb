edcd402588ce7346d8cd78d75b3ac83cad8dc68f
vivekp <vivekp@edabc166-3b47-0410-8d92-8c1ea7f5640b>
2008-06-23 00:39:28 +0000

    OSGi is default but GlassFish nucleus does not have all the dependencies for OSGi and hence it fails. For now starting GlassFish in HK2.
    git-svn-id: https://svn.dev.java.net/svn/glassfish-scripting/trunk/rails/v3/gem@173 edabc166-3b47-0410-8d92-8c1ea7f5640b

--- glassfish.rb
+++ glassfish.rb
@@ -3,6 +3,9 @@ require 'glassfish-@GLASSFISH_VERSION@.jar'
 module GlassFish
   import com.sun.enterprise.glassfish.bootstrap.ASMain
   def startup(args)
+    #OSGi is default but GlassFish nucleus does not have all the dependencies for OSGi and
+    #hence it fails. For now running it in HK2
+    java.lang.System.setProperty("GlassFish_Platform", "HK2");
     ASMain.main(args.to_java(:string))
   end
 
