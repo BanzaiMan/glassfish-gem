3cbd309d3a5e54e4f63d2265c7f315c4c33443b9
vivekp <vivekp@edabc166-3b47-0410-8d92-8c1ea7f5640b>
2008-09-09 22:47:56 +0000

    Fix for https://glassfish.dev.java.net/issues/show_bug.cgi?id=4823 and other minor fixes.
    git-svn-id: https://svn.dev.java.net/svn/glassfish-scripting/trunk/rails/v3/gem@372 edabc166-3b47-0410-8d92-8c1ea7f5640b

--- glassfish.rb
+++ glassfish.rb
@@ -1,4 +1,4 @@
-require 'glassfish-@GLASSFISH_VERSION@.jar'
+require 'glassfish-10.0-SNAPSHOT.jar'
 
 module GlassFish
   import com.sun.enterprise.glassfish.bootstrap.ASMain
@@ -6,7 +6,11 @@ module GlassFish
     #OSGi is default but GlassFish nucleus does not have all the dependencies for OSGi and
     #hence it fails. For now running it in HK2
     java.lang.System.setProperty("GlassFish_Platform", "HK2");
-    ASMain.main(args.to_java(:string))
+
+    #set jruby runtime property
+    java.lang.System.setProperty("jruby.runtime", args[:runtimes].to_s)
+
+    ASMain.main([args[:app_dir], "--contextroot", args[:contextroot]].to_java(:string))
   end
 
   module_function :startup
